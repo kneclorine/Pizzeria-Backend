2d5543c98e79adc834d5a37520ddc60c27c0a844
Alexander García <alexander.garcia-mas@capgemini.com>
2021-08-17 16:50:07 +0200

    Conversión a BigDecimal

--- demo/src/main/java/com/example/demo/application/ingredientApplication/IngredientDTO.java
+++ demo/src/main/java/com/example/demo/application/ingredientApplication/IngredientDTO.java
@@ -1,11 +1,12 @@
 package com.example.demo.application.ingredientApplication;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public class IngredientDTO implements Serializable{
     
     public UUID id;
     public String name;
-    public double price;
+    public BigDecimal price;
 }
