513400086c3518c4e850f845e8bf7d6b5c8de11e
Alexander García <alexander.garcia-mas@capgemini.com>
2021-08-20 14:09:17 +0200

    Limpieza de main, añadidas dependecias de logger e implementación de la configuración de XML

--- demo/src/main/java/com/example/demo/application/ingredientApplication/IngredientApplicationImp.java
+++ demo/src/main/java/com/example/demo/application/ingredientApplication/IngredientApplicationImp.java
@@ -6,6 +6,7 @@ import com.example.demo.domain.ingredientDomain.Ingredient;
 import com.example.demo.domain.ingredientDomain.IngredientRepository;
 
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,11 +15,13 @@ public class IngredientApplicationImp implements IngredientApplication {
 
     private final IngredientRepository ingredientRepository;
     private final ModelMapper modelMapper;
+    private final Logger logger;
 
     @Autowired
-    public IngredientApplicationImp(IngredientRepository ingredientRepository){
+    public IngredientApplicationImp(final IngredientRepository ingredientRepository, final Logger logger){
         this.ingredientRepository = ingredientRepository;
         this.modelMapper = new ModelMapper();
+        this.logger = logger;
     }
 
     @Override
--- demo/src/main/java/com/example/demo/domain/ingredientDomain/Ingredient.java
+++ demo/src/main/java/com/example/demo/domain/ingredientDomain/Ingredient.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import com.example.demo.core.Entities;
@@ -17,11 +17,11 @@ import lombok.Setter;
 @Entity
 public @NoArgsConstructor @Getter @Setter class Ingredient extends Entities{
 
-    @NotBlank @Size(min=2, max=255)
+    @NotNull @Size(min=2, max=255)
     @Column(nullable = false, unique = true)
     private String name;
 
-    @NotBlank
+    @NotNull
     @Column(nullable = false, precision = 5, scale = 2)
     @Digits(integer = 5, fraction = 2)
     private BigDecimal price;
