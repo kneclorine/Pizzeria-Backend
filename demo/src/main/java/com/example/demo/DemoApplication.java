44f5d21dc1469cbab5ff1e2f8ddde1f8655b0929
Alexander García <alexander.garcia-mas@capgemini.com>
2021-08-29 00:02:40 +0200

    Implementación del Token

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,15 @@
 package com.example.demo;
 
+import com.example.demo.security.JWTAuthorizationFilter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SpringBootApplication
 public class DemoApplication{
@@ -10,18 +18,18 @@ public class DemoApplication{
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	// @EnableWebSecurity
-	// @Configuration
-	// class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	// 	@Override
-	// 	protected void configure(HttpSecurity http) throws Exception {
-	// 		http.csrf().disable()
-	// 			.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
-	// 			.authorizeRequests()
-	// 			.antMatchers(HttpMethod.POST, "/user/").permitAll()
-	// 			.antMatchers(HttpMethod.GET, "/user/").hasRole("USER")
-	// 			.anyRequest().authenticated();
-	// 	}
-	// }
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
+				.antMatchers(HttpMethod.GET, "/api/v1/users").hasRole("USER")
+				.anyRequest().authenticated();
+		}
+	}
 }
