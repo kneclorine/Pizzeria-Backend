c7628a5af270fb297094e6789e5354b7259bea7c
carloscastillogarcia <krestinho@gmail.com>
2021-10-04 20:01:16 +0200

    fixed ingredient

--- demo/src/main/java/com/example/demo/controller/IngredientController.java
+++ demo/src/main/java/com/example/demo/controller/IngredientController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api/v1/ingredients")
