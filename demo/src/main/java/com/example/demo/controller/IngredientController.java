dc31fa9877730ff1371f2d99e09fd298513646a3
Alexander <59999903+kneclorine@users.noreply.github.com>
2021-10-04 18:30:44 +0200

    CrossOrigin added

--- demo/src/main/java/com/example/demo/controller/IngredientController.java
+++ demo/src/main/java/com/example/demo/controller/IngredientController.java
@@ -11,6 +11,7 @@ import com.example.demo.application.ingredientApplication.IngredientDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1/ingredients")
 public class IngredientController{
--- demo/src/main/java/com/example/demo/controller/PizzaController.java
+++ demo/src/main/java/com/example/demo/controller/PizzaController.java
@@ -11,6 +11,7 @@ import com.example.demo.application.pizzaApplication.PizzaDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1/pizzas")
 public class PizzaController {
