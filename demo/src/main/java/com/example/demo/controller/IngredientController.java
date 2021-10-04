7716feb301f3f14ad0f7cdaa70fa876980392afc
carloscastillogarcia <krestinho@gmail.com>
2021-10-04 20:00:17 +0200

    fixed IngredientController

--- demo/src/main/java/com/example/demo/controller/IngredientController.java
+++ demo/src/main/java/com/example/demo/controller/IngredientController.java
@@ -11,6 +11,7 @@ import com.example.demo.application.ingredientApplication.IngredientDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api/v1/ingredients")
 public class IngredientController{
     
